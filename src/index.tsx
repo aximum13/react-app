27ed800cdd289bc6dd85094b11cd11fc44770f18
aximum13 <aximum2@yandex.ru>
2023-10-11 22:16:39 +0300

    update name store

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import App from './App';
-import store from './store/store';
+import App from 'App';
+import store from 'store';
 import { Provider } from 'react-redux';
 
 const root = ReactDOM.createRoot(
---
+++ src/store/index.ts
@@ -0,0 +1,24 @@
+import { configureStore, combineReducers } from '@reduxjs/toolkit';
+import { save } from 'redux-localstorage-simple';
+
+import songReducer from 'models/songs/slices/songsSlice';
+import filterReducer from 'models/filters/slices/filtersSlice';
+
+const saveSong = save({
+  namespace: 'musicList',
+});
+
+const reducers = combineReducers({
+  songs: songReducer,
+  filters: filterReducer,
+});
+
+const store = configureStore({
+  reducer: reducers,
+  middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(saveSong),
+});
+
+export default store;
+
+export type RootState = ReturnType<typeof store.getState>;
+export type AppDispatch = typeof store.dispatch;
