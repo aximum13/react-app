a7779cabd1e19ee269bb432654f1d5cfc5ebeefc
aximum13 <aximum2@yandex.ru>
2023-10-17 14:49:49 +0300

    updated store

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,20 +1,14 @@
-import { configureStore, combineReducers } from '@reduxjs/toolkit';
+import { configureStore } from '@reduxjs/toolkit';
 import { save } from 'redux-localstorage-simple';
 
 import songReducer from 'models/songs/slices/songsSlice';
-import filterReducer from 'models/filters/slices/filtersSlice';
 
 const saveSong = save({
   namespace: 'musicList',
 });
 
-const reducers = combineReducers({
-  songs: songReducer,
-  filters: filterReducer,
-});
-
 const store = configureStore({
-  reducer: reducers,
+  reducer: songReducer,
   middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(saveSong),
 });
 
